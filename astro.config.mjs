7a5b9cbb330e5bfd82f697e8d245986447ba199b
Raphael Sanchez <hello@raphaelsanchez.design>
2025-01-31 15:37:08 +0100

    ajout de la dépendance astro-icon et configuration de l'intégration dans astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [icon()],
+})
